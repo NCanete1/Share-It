fd33079115a013ab07f8c96a9352039d764bcd69
nico <n.canete01@ufromail.cl>
2022-05-31 21:04:27 -0400

    6

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -2,82 +2,85 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Menu {
-    int opcion=0;
-    Biblioteca biblioteca= new Biblioteca();
+
+    Biblioteca biblioteca = new Biblioteca();
+    Project2 project = new Project2();
     private ArrayList<Usuario> usuariolist = new ArrayList();
-    private ArrayList<Biblioteca> bibliotecaArrayList= new ArrayList();
-    Scanner teclado= new Scanner(System.in);
-    public void MenuPrincipal(){
-        do {
-            try {
-                System.out.println("0. Mostrar Usuarios");
-                System.out.println("1.Añadir Usuario");
-                opcion = teclado.nextInt();
+    private ArrayList<Biblioteca> bibliotecaArrayList = new ArrayList();
+    Scanner teclado = new Scanner(System.in);
+    int opcion = 0;
+
+    public void MenuPrincipal() {
+
+        Scanner sn = new Scanner(System.in);
+        int opcion = 0;
+        try {
+            do {
+                System.out.println("Bienvenido!");
+                System.out.println("---------------");
+                System.out.println("Mostrar usuarios [1]");
+                System.out.println("Agregar usuarios [2]");
+                System.out.println("Ingresar como... [3]");
+                System.out.println("---------------");
+                opcion = sn.nextInt();
                 switch (opcion) {
-                    case 0: //Mostrar Usuarios
-                        System.out.println(getUsuarios());
-                        MenuBiblioteca(0);
+                    case 1:
+                        project.MostrarBibliotecaUsuarios();
                         break;
-                    case 1: //Crear Usuario
-                        UsuariosTest();
+                    case 2:
+                        String nuevouser= teclado.nextLine();
+                        project.LibreriaUsuario(nuevouser);
                         break;
+                    case 3:
+                        System.out.println("Introduzca el nombre del usuario a entrar: ");
+                        String user= teclado.nextLine();
+                        MenuBiblioteca(user);
+                        break;
+                    default:
+                        System.out.println("Ingrese un numero valido");
+                        System.out.println("-------------------------");
                 }
-            } catch (Exception e) {
-                System.out.println("Error en MenuPrincipal");
+            } while (opcion >= 1 && opcion <= 4);
+
+        } catch (Exception e) {
+            System.out.println("Ingrese un numero valido");
+            System.out.println("-------------------------");
+        }
+    }
+
+    public void MenuUsuarios() {
+        System.out.println("1. Usuario 1");
+        System.out.println("2. Usuario 2");
+        opcion = teclado.nextInt();
+        switch (opcion) {
+            case 1 -> //Usuario 1
+                    MenuBiblioteca("usuario 1");
+            case 2 -> //Usuario 2
+                    MenuBiblioteca("usuario 2");
+        }
+    }
+
+    public void MenuBiblioteca(String Usuario) {
+        do {
+            System.out.println("Mostrar libros de Usuarios [1]");
+            System.out.println("Agregar Libro [2]");
+            System.out.println("Buscar Libro [3]");
+            opcion = teclado.nextInt();
+            switch (opcion) {
+                case 1: //Mostrar Libros
+                    System.out.println("Mostrando libros Usuario:");
+                    project.LeerBiblioteca(Usuario);
+                    break;
+                case 2: //Añadir Libro
+                    System.out.println("Añadiendo Libro");
+                    project.AddLibrosBiblioteca(Usuario);
+                    break;
+                case 3:
+                    String libro = teclado.nextLine();
+                    project.BuscarLibro(libro);
+                    break;
             }
-        }while (opcion!=-1);
-    }
-    public void MenuUsuarios (){
-        System.out.println("0. Uusario 1");
-        System.out.println("1. Usuario 2");
-        opcion= teclado.nextInt();
-        switch (opcion) {
-            case 0 -> //Usuario 1
-                    MenuBiblioteca(0);
-            case 1 -> //Usuario 2
-                    MenuBiblioteca(1);
-        }
-    }
-    public void MenuBiblioteca(int n){
-        System.out.println("0. Mostrar libros");
-        System.out.println("1. Añadir Libro");
-        opcion= teclado.nextInt();
-        switch (opcion) {
-            case 0: //Mostrar Libros
-                System.out.println("Mostrar libro:");
-                System.out.println(usuariolist.get(n).getBibliotecas().get(n).getLibros());
-                break;
-            case 1: //Añadir Libro
-                System.out.println("Añadiendo Libro");
-
-                break;
-        }
-    }
-
-    /* AQUI NO VA CREAR USUARIOS CAMBIAR */
-
-
-     public void addUsuario(String nombre, String correo, String telefono){
-         Usuario usuario= new Usuario();
-         usuario.setNombre(nombre);
-         usuario.setCorreo(correo);
-         usuario.setTelefono(telefono);
-         usuario.setBibliotecas(bibliotecaArrayList);
-         usuariolist.add(usuario);
-    }
-
-    public void UsuariosTest(){
-        addUsuario("Juan","jorge@gmail.com","+56948257");
-        addUsuario("Pepe","jorge2@gmail.com","+56948257");
-        addUsuario("Jorge","jorge3@gmail.com","+56948257");
-    }
-
-    public String getUsuarios(){
-        String lista= "";
-        for(Usuario usuario: usuariolist){
-            lista += usuario+ "\n";
-        }
-        return lista;
+        }while (opcion >= 1 && opcion <= 4);
     }
 
 }
