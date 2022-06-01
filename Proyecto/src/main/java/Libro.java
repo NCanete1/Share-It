e633bc0d248183ea133d70791b2433d6107b83a4
NCanete1 <101651736+NCanete1@users.noreply.github.com>
2022-05-31 21:55:50 -0400

    Avance 02

---
+++ Proyecto/src/main/java/Libro.java
@@ -0,0 +1,39 @@
+public class Libro {
+    private String titulo, autor , genero;
+
+    public Libro() {
+        this.titulo = "";
+        this.autor = "";
+        this.genero = "";
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    @Override
+    public String toString() {
+        return this.titulo+", "+this.autor+", "+this.genero;
+    }
+}
+
